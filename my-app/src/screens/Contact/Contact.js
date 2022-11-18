2b3bd692dd7369b251ef345fd645f260f229ef26
haclesrocha200@gmail.com <71661551+hacles100@users.noreply.github.com>
2022-11-18 12:06:08 -0100

    pages done

--- my-app/src/screens/Contact/Contact.js
+++ my-app/src/screens/Contact/Contact.js
@@ -1,7 +1,32 @@
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import {  green } from '@mui/material/colors';
+import './Contact.css'
+
 
 export default function Contact() {
 
-    return <>
-        <h1>Contact screen</h1>
-    </>
+    return <div className='container'>
+        <h1>Contact List</h1>
+
+        <div className='main'>
+
+          <div className='first'>
+            <Avatar alt="Neymar"  sx={{ bgcolor: green[500], width: 50, height: 50  }}/>
+            <span>Menino Ney</span>
+          </div>
+
+          <div className='second'>
+            <Avatar alt="Neymar"  sx={{ bgcolor: green[500], width: 50, height: 50  }}/>
+            <span>Neymar Junior</span>
+          </div>
+
+          <div className='third'>
+            <Avatar alt="Neymar"  sx={{ bgcolor: green[500], width: 50, height: 50  }}/>
+            <span>Hericles Rocha</span>
+          </div>
+        </div>
+      
+
+    </div>
 }
--- my-app/src/screens/Dial/Dial.js
+++ my-app/src/screens/Dial/Dial.js
@@ -1,8 +1,9 @@
 
 import * as React from 'react';
 import Avatar from '@mui/material/Avatar';
-import Stack from '@mui/material/Stack';
 import {  green } from '@mui/material/colors';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
 import './Dial.css'
 
 export default function Dial() {
@@ -13,20 +14,36 @@ export default function Dial() {
         <h1>5850628</h1>
 
         <div className='numbers'>
-        <Stack direction="row" spacing={2}>
-         <Avatar sx={{ bgcolor: green[500] }}>1</Avatar>
-         <Avatar sx={{ bgcolor: green[500] }}>2</Avatar>
-         <Avatar sx={{ bgcolor: green[500] }}>3</Avatar>
-         <Avatar sx={{ bgcolor: green[500] }}>4</Avatar>
-         <Avatar sx={{ bgcolor: green[500] }}>5</Avatar>
-         <Avatar sx={{ bgcolor: green[500] }}>6</Avatar>
-         <Avatar sx={{ bgcolor: green[500] }}>7</Avatar>
-         <Avatar sx={{ bgcolor: green[500] }}>8</Avatar>
-         <Avatar sx={{ bgcolor: green[500] }}>9</Avatar>
-         <Avatar sx={{ bgcolor: green[500] }}>*</Avatar>
-         <Avatar sx={{ bgcolor: green[500] }}>0</Avatar>
-         <Avatar sx={{ bgcolor: green[500] }}>#</Avatar>
-        </Stack>
+            <div className='numbers-1'>
+              <Avatar sx={{ bgcolor: green[500], width: 60, height: 60 }}>1</Avatar>
+              <Avatar sx={{ bgcolor: green[500], width: 60, height: 60  }}>2</Avatar>
+              <Avatar sx={{ bgcolor: green[500], width: 60, height: 60  }}>3</Avatar>
+            </div>
+          
+          <div className='numbers-2'>
+           <Avatar sx={{ bgcolor: green[500], width: 60, height: 60  }}>4</Avatar>
+           <Avatar sx={{ bgcolor: green[500], width: 60, height: 60  }}>5</Avatar>
+           <Avatar sx={{ bgcolor: green[500], width: 60, height: 60  }}>6</Avatar>
+          </div>
+          
+          <div className='numbers-3'>
+           <Avatar sx={{ bgcolor: green[500], width: 60, height: 60  }}>7</Avatar>
+           <Avatar sx={{ bgcolor: green[500], width: 60, height: 60  }}>8</Avatar>
+           <Avatar sx={{ bgcolor: green[500], width: 60, height: 60  }}>9</Avatar>
+          </div>
+          
+          <div className='numbers-4'>
+           <Avatar sx={{ bgcolor: green[500], width: 60, height: 60  }}>*</Avatar>
+           <Avatar sx={{ bgcolor: green[500], width: 60, height: 60  }}>0</Avatar>
+           <Avatar sx={{ bgcolor: green[500], width: 60, height: 60  }}>#</Avatar>
+          </div>
+
+          <div className='phone'>
+            <BottomNavigationAction label="Dial" icon={<LocalPhoneIcon />} sx={{width: 60, height: 60 }} />
+          </div>
+
+          
+      
         </div>
       
     
