43dafc74b5d48c3746c899fe89633b2e84722368
haclesrocha200@gmail.com <71661551+hacles100@users.noreply.github.com>
2022-11-18 21:51:00 -0100

    update

--- my-app/src/screens/Contact/Contact.js
+++ my-app/src/screens/Contact/Contact.js
@@ -1,32 +1,105 @@
 import * as React from 'react';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import RestoreIcon from '@mui/icons-material/Restore';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ArchiveIcon from '@mui/icons-material/Archive';
+import Paper from '@mui/material/Paper';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import ListItemText from '@mui/material/ListItemText';
 import Avatar from '@mui/material/Avatar';
-import {  green } from '@mui/material/colors';
+
 import './Contact.css'
 
 
-export default function Contact() {
+const messageExamples = [
+  {
+    primary: 'Menino Ney',
+    secondary: 5850628,
+    person: '/static/images/avatar/5.jpg',
+  },
+  {
+    primary: 'Neymar Junior',
+    secondary: 5850628,
+    person: '/static/images/avatar/5.jpg',
+  },
+  {
+    primary: 'Hericles Rocha',
+    secondary: 5850628,
+    person: '/static/images/avatar/5.jpg',
+  },
+  {
+    primary: 'Mana Rocha',
+    secondary: 5850628,
+    person: '/static/images/avatar/5.jpg',
+  },
+  {
+    primary: 'Emerson Barros',
+    secondary: 5850628,
+    person: '/static/images/avatar/5.jpg',
+  },
+  {
+    primary: 'Lisa Luz',
+    secondary: 5850628,
+    person: '/static/images/avatar/5.jpg',
+  },
+  {
+    primary: 'Phil Poden',
+    secondary: 5850628,
+    person: '/static/images/avatar/5.jpg',
+  },
 
-    return <div className='container'>
-        <h1>Contact List</h1>
+];
 
-        <div className='main'>
+function refreshMessages() {
+  const getRandomInt = (max) => Math.floor(Math.random() * Math.floor(max));
 
-          <div className='first'>
-            <Avatar alt="Neymar"  sx={{ bgcolor: green[500], width: 50, height: 50  }}/>
-            <span>Menino Ney</span>
-          </div>
-
-          <div className='second'>
-            <Avatar alt="Neymar"  sx={{ bgcolor: green[500], width: 50, height: 50  }}/>
-            <span>Neymar Junior</span>
-          </div>
-
-          <div className='third'>
-            <Avatar alt="Neymar"  sx={{ bgcolor: green[500], width: 50, height: 50  }}/>
-            <span>Hericles Rocha</span>
-          </div>
-        </div>
-      
-
-    </div>
+  return Array.from(new Array(50)).map(
+    () => messageExamples[getRandomInt(messageExamples.length)],
+  );
 }
+
+export default function Contact() {
+  const [value, setValue] = React.useState(0);
+  const ref = React.useRef(null);
+  const [messages, setMessages] = React.useState(() => refreshMessages());
+
+  React.useEffect(() => {
+    ref.current.ownerDocument.body.scrollTop = 0;
+    setMessages(refreshMessages());
+  }, [value, setMessages]);
+
+  return (
+    <Box sx={{ pb: 7 }} ref={ref}>
+      <CssBaseline />
+      <List>
+        {messages.map(({ primary, secondary, person }, index) => (
+          <ListItem button key={index + person}>
+            <ListItemAvatar>
+              <Avatar alt="Profile Picture" src={person} />
+            </ListItemAvatar>
+            <ListItemText primary={primary} secondary={secondary} />
+          </ListItem>
+        ))}
+      </List>
+      <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
+        <BottomNavigation
+          showLabels
+          value={value}
+          onChange={(event, newValue) => {
+            setValue(newValue);
+          }}
+        >
+          <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
+          <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
+          <BottomNavigationAction label="Archive" icon={<ArchiveIcon />} />
+        </BottomNavigation>
+      </Paper>
+    </Box>
+  );
+}
+
--- my-app/src/screens/Dial/Dial.js
+++ my-app/src/screens/Dial/Dial.js
@@ -1,52 +1,82 @@
-
+import { useState } from 'react';
 import * as React from 'react';
 import Avatar from '@mui/material/Avatar';
 import {  green } from '@mui/material/colors';
 import BottomNavigationAction from '@mui/material/BottomNavigationAction';
 import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
 import './Dial.css'
+import BackspaceIcon from '@mui/icons-material/Backspace';
+import sound from './Djodje.mp3'
+
+
 
 export default function Dial() {
 
 
+ 
+
+    const [display, setDisplay] = useState('');
+
+    function add(event) {
+        const newDisplayValue = display + event.target.textContent;
+        setDisplay(newDisplayValue);
+    }
+
+
+     function clear() {
+        setDisplay('');
+     }
+
+
+     function playAudio() {
+        sound.play();
+    }
+
+     function chamada(){
+
+        playAudio()
+     }
+
+
     return <div className="container">
 
-        <h1>5850628</h1>
+        <h1>{display}     <BackspaceIcon onClick={clear}></BackspaceIcon></h1>
+
 
         <div className='numbers'>
             <div className='numbers-1'>
-              <Avatar sx={{ bgcolor: green[500], width: 60, height: 60 }}>1</Avatar>
-              <Avatar sx={{ bgcolor: green[500], width: 60, height: 60  }}>2</Avatar>
-              <Avatar sx={{ bgcolor: green[500], width: 60, height: 60  }}>3</Avatar>
-            </div>
+       
+              <Avatar sx={{ bgcolor: green[500], width: 60, height: 60 }} onClick={add}>1</Avatar>
+              <Avatar sx={{ bgcolor: green[500], width: 60, height: 60  }} onClick={add}>2</Avatar>
+              <Avatar sx={{ bgcolor: green[500], width: 60, height: 60  }} onClick={add}>3</Avatar>      
+        
+            </div>    
           
           <div className='numbers-2'>
-           <Avatar sx={{ bgcolor: green[500], width: 60, height: 60  }}>4</Avatar>
-           <Avatar sx={{ bgcolor: green[500], width: 60, height: 60  }}>5</Avatar>
-           <Avatar sx={{ bgcolor: green[500], width: 60, height: 60  }}>6</Avatar>
+           <Avatar sx={{ bgcolor: green[500], width: 60, height: 60  }} onClick={add}>4</Avatar>
+           <Avatar sx={{ bgcolor: green[500], width: 60, height: 60  }} onClick={add}>5</Avatar>
+           <Avatar sx={{ bgcolor: green[500], width: 60, height: 60  }} onClick={add}>6</Avatar>
           </div>
           
           <div className='numbers-3'>
-           <Avatar sx={{ bgcolor: green[500], width: 60, height: 60  }}>7</Avatar>
-           <Avatar sx={{ bgcolor: green[500], width: 60, height: 60  }}>8</Avatar>
-           <Avatar sx={{ bgcolor: green[500], width: 60, height: 60  }}>9</Avatar>
+           <Avatar sx={{ bgcolor: green[500], width: 60, height: 60  }} onClick={add}>7</Avatar>
+           <Avatar sx={{ bgcolor: green[500], width: 60, height: 60  }} onClick={add}>8</Avatar>
+           <Avatar sx={{ bgcolor: green[500], width: 60, height: 60  }} onClick={add}>9</Avatar>
           </div>
           
           <div className='numbers-4'>
-           <Avatar sx={{ bgcolor: green[500], width: 60, height: 60  }}>*</Avatar>
-           <Avatar sx={{ bgcolor: green[500], width: 60, height: 60  }}>0</Avatar>
-           <Avatar sx={{ bgcolor: green[500], width: 60, height: 60  }}>#</Avatar>
+           <Avatar sx={{ bgcolor: green[500], width: 60, height: 60  }} onClick={add}>*</Avatar>
+           <Avatar sx={{ bgcolor: green[500], width: 60, height: 60  }} onClick={add}>0</Avatar>
+           <Avatar sx={{ bgcolor: green[500], width: 60, height: 60  }} onClick={add}>#</Avatar>
           </div>
+   
 
           <div className='phone'>
-            <BottomNavigationAction label="Dial" icon={<LocalPhoneIcon />} sx={{width: 60, height: 60 }} />
+            <BottomNavigationAction label="Dial" icon={<LocalPhoneIcon />} sx={{width: 60, height: 60 }} onClick={chamada}/>
           </div>
-
-          
       
         </div>
-      
     
-        
+
     </div>
 }
